ecf8517c4f77dfa0d6570778a73417dbccebe4e4
Young Seok Tony Kim <mightydeveloper@gmail.com>
2015-12-21 18:55:05 +0900

    Edited some whitespaces. Added line typedef struct to represent cache. Edited xdump function to allow multiple word block size.

--- lab4/cache_output_format.c
+++ lab4/cache_output_format.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define BYTES_PER_WORD 4
+#define MAX_BLOCK_WORD 8
+
+typedef struct line {
+    bool valid;
+    uint32_t content[MAX_BLOCK_WORD];   // tag bit + index bit (block offset masked by 0)
+    int used;                           // used count for LRU
+} line;
+
+
+
 
 /***************************************************************/
 /*                                                             */
@@ -14,7 +25,7 @@
 void cdump(int capacity, int assoc, int blocksize){
 
 	printf("Cache Configuration:\n");
-    	printf("-------------------------------------\n");
+    printf("-------------------------------------\n");
 	printf("Capacity: %dB\n", capacity);
 	printf("Associativity: %dway\n", assoc);
 	printf("Block Size: %dB\n", blocksize);
@@ -25,13 +36,13 @@ void cdump(int capacity, int assoc, int blocksize){
 /*                                                             */
 /* Procedure : sdump                                           */
 /*                                                             */
-/* Purpose   : Dump cache stat		                       */   
+/* Purpose   : Dump cache stat                                 */
 /*                                                             */
 /***************************************************************/
 void sdump(int total_reads, int total_writes, int write_backs,
 	int reads_hits, int write_hits, int reads_misses, int write_misses) {
 	printf("Cache Stat:\n");
-    	printf("-------------------------------------\n");
+    printf("-------------------------------------\n");
 	printf("Total reads: %d\n", total_reads);
 	printf("Total writes: %d\n", total_writes);
 	printf("Write-backs: %d\n", write_backs);
@@ -48,13 +59,13 @@ void sdump(int total_reads, int total_writes, int write_backs,
 /* Procedure : xdump                                           */
 /*                                                             */
 /* Purpose   : Dump current cache state                        */ 
-/* 							       */
-/* Cache Design						       */
-/*  							       */
-/* 	    cache[set][assoc][word per block]		       */
-/*      						       */
-/*      						       */
-/*       ----------------------------------------	       */
+/*                                                             */
+/* Cache Design                                                */
+/*                                                             */
+/* 	    cache[set][assoc][word per block]                      */
+/*                                                             */
+/*                                                             */
+/*       ----------------------------------------              */
 /*       I        I  way0  I  way1  I  way2  I                 */
 /*       ----------------------------------------              */
 /*       I        I  word0 I  word0 I  word0 I                 */
@@ -67,22 +78,25 @@ void sdump(int total_reads, int total_writes, int write_backs,
 /*       I        I  word2 I  word2 I  word2 I                 */
 /*       I        I  word3 I  word3 I  word3 I                 */
 /*       ----------------------------------------              */
-/*      						       */
+/*                                                             */
 /*                                                             */
 /***************************************************************/
-void xdump(int set, int way, uint32_t** cache)
+void xdump(int set, int way, int words, line** cache)
 {
 	int i,j,k = 0;
 
 	printf("Cache Content:\n");
-    	printf("-------------------------------------\n");
+    printf("-------------------------------------\n");
 	for(i = 0; i < way;i++)
 	{
 		if(i == 0)
 		{
-			printf("    ");
+			printf("          ");
 		}
-		printf("      WAY[%d]",i);
+		printf("WAY[%d]      ",i);
+        for (k = 0; k < words-1; k++) {
+            printf("            ");
+        }
 	}
 	printf("\n");
 
@@ -91,11 +105,10 @@ void xdump(int set, int way, uint32_t** cache)
 		printf("SET[%d]:   ",i);
 		for(j = 0; j < way;j++)
 		{
-			if(k != 0 && j == 0)
-			{
-				printf("          ");
-			}
-			printf("0x%08x  ", cache[i][j]);
+            for (k = 0; k < words; k++) {
+                printf("0x%08x  ", cache[i][j].content[k]);
+            }
+			
 		}
 		printf("\n");
 	}
@@ -107,28 +120,33 @@ void xdump(int set, int way, uint32_t** cache)
 
 int main(int argc, char *argv[]) {                              
 
-	uint32_t** cache;
+	line** cache;
 	int i, j, k;	
-	int capacity = 256;
-	int way = 4;
-	int blocksize = 8;
-	int set = capacity/way/blocksize;
-	int words = blocksize / BYTES_PER_WORD;	
+	int capacity = 256;                     // capacity in Bytes
+	int way = 4;                            // associativity
+	int blocksize = 8;                      // block size in Bytes
+	int set = capacity/way/blocksize;       // index
+	int words = blocksize / BYTES_PER_WORD;	// #of words per block.
 
 	// allocate
-	cache = (uint32_t**) malloc (sizeof(uint32_t*) * set);
+    cache = (line**) malloc(sizeof(line*) * set);
+    for(i = 0; i < set; i++) {
+        cache[i] = (struct line*) malloc(sizeof(line) * way);
+    }
+    
+    // init
 	for(i = 0; i < set; i++) {
-		cache[i] = (uint32_t*) malloc(sizeof(uint32_t) * way);
-	}
-	for(i = 0; i < set; i++) {
-		for(j = 0; j < way; j ++) 
-			cache[i][j] = 0x0;
+        for(j = 0; j < way; j ++) {
+            for (k = 0; k < words; k++) {
+                cache[i][j].content[k] = 0x0;
+            }
+        }
 	}
 
 	// test example
-    	cdump(capacity, way, blocksize);
-    	sdump(0, 0, 0, 0, 0, 0, 0); 
-    	xdump(set, way, cache);
+    cdump(capacity, way, blocksize);
+    sdump(0, 0, 0, 0, 0, 0, 0);
+    xdump(set, way, words, cache);
 
-    	return 0;
+    return 0;
 }
